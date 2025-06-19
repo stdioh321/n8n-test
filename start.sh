07fc58eeba2a46b854b82a880e3436935823d4ca
Henrique <henrique.dias@gruponewway.com.br>
2025-06-19 13:01:40 -0300

    chore(main): update start.sh
    # Files
    NEW:
    EDITED: start.sh
    DELETED:

--- start.sh
+++ start.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 echo "# Iniciando start #####"
-DOCKER_COMPOSE="$(command -v docker-compose || command -v "docker compose")"
+if command -v docker-compose &>/dev/null; then
+    DOCKER_COMPOSE="docker-compose"
+elif command -v docker &>/dev/null && docker compose version &>/dev/null; then
+    DOCKER_COMPOSE="docker compose"
+else
+    echo "Error: docker-compose or 'docker compose' not found."
+    exit 1
+fi
 
 function cleanup_and_exit() {
     echo "# Removendo containers"
@@ -22,8 +29,8 @@ monitor_terminal() {
 
 trap 'cleanup_and_exit' SIGINT
 {
-    cd infra ;
-    docker build -f Dockerfile.base . -t n8n-base ;
+    cd infra
+    docker build -f Dockerfile.base . -t n8n-base
 }
 
 $DOCKER_COMPOSE build
